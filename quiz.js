1eaced6186c817e94fb6ce111b06d1c8ce9ebb7b
Darshil Dixit <this.is.darshil@gmail.com>
2021-09-30 16:26:58 +0530

    update

---
+++ quiz.js
@@ -0,0 +1,20 @@
+var quizForm = document.querySelector(".quiz-form");
+var submitButton = document.querySelector("#submit-button");
+var outputScore = document.querySelector(".output-msg");
+
+var correctAnswers = ['90°', 'Right angled', 'Equilateral triangle'];
+
+function calculateScore() {
+    var formResults = new FormData(quizForm);
+    let score = 0,
+      index = 0;
+    for (let value of formResults.values()) {
+      if (value === correctAnswers[index]) {
+        score = score + 1;
+      }
+      index = index + 1;
+    }
+    outputScore.innerText = "The score is " + score;
+  }
+  
+  submitButton.addEventListener("click", calculateScore)
