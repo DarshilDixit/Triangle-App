fc810bb5e5a8a373366d1238d901152745ef1512
Darshil Dixit <this.is.darshil@gmail.com>
2021-09-30 15:40:07 +0530

    chore

---
+++ app.js
@@ -0,0 +1,21 @@
+var angleInput = document.querySelectorAll(".angle-container");
+var checkButton = document.querySelector("#check-button");
+var outputMsg = document.querySelector("#output-msg");
+
+
+
+checkButton.addEventListener("click", outputHandler);
+
+function outputHandler() {
+    if (calculateSumOfAngles() === 180) {
+        outputMsg.innerText = 'Yayy, the angles form a triangle';
+    } else {
+        outputMsg.innerText = 'Oh Oh!, the angles do not form a triangle';
+    }
+}
+
+function calculateSumOfAngles() {
+    var sumOfAngles =
+        Number(angleInput[0].value) + Number(angleInput[1].value) + Number(angleInput[2].value);
+    return sumOfAngles;
+}
